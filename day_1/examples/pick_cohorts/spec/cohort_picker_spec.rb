4082706bf96b7f4b8bcc6b913e59c6d086bd4a9c
Cory Mathis <corymathis@MacBook-Pro-3.local>
2022-05-23 12:05:17 -0500

    Bootstrap a new rails project.  Generate our Network model.

--- day_1/examples/pick_cohorts/spec/cohort_picker_spec.rb
+++ day_1/examples/pick_cohorts/spec/cohort_picker_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CohortPicker do
   it "divides into two cohorts by default" do 
     picker = CohortPicker.new(students: "Cory, Michelle, Sammy, Walt, Hazel, Charlotte")
     cohorts = picker.run
-
+  
     expect(cohorts.count).to eq(2)
   end
 end
