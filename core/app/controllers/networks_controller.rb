193a62c369cc2f4d6dfd6b390d2d9ecadd7eac61
Cory Mathis <corymathis@MacBook-Pro-3.local>
2022-05-25 10:00:06 -0500

    Flesh out networks controller a bit.

--- core/app/controllers/networks_controller.rb
+++ core/app/controllers/networks_controller.rb
@@ -1,6 +1,38 @@
 class NetworksController < ApplicationController
 
+    def index 
+        @networks = Network.where(network_params)
+        render json: params
+    end
+
     def show
         @network = Network.find(params[:id])
+
+        render json: @network
     end
+
+    def edit 
+    end
+
+    def update
+    end
+    
+    def new 
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    private 
+
+    def network_params 
+        params.require(:network).permit(
+            :name,
+            :state
+        )
+    end
+
 end
